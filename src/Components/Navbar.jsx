ffacc18a0f15849ff315d41fb324fdc705b5ac2c
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-05-10 20:20:38 +0530

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <header className="ull py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width ">
-        <img src={appleImg} alt="apple" width={14} height={18} />
+        <a href="/">
+          <img src={appleImg} alt="apple" width={14} height={18} />
+        </a>
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
             <div
